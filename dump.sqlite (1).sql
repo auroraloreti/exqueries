5404469bb1efcb198dcaf0d2caf544e19d44906f
auroraloreti <loretiaurora16@gmail.com>
2024-08-15 17:37:44 +0200

    esercizio svolto

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'Titanic' ('PassengerId' INTEGER,'Survived' INTEGER,'Pclass' INTEGER,'Name' TEXT,'Sex' TEXT,'Age' REAL,'SibSp' INTEGER,'Parch' INTEGER,'Ticket' TEXT,'Fare' REAL,'Cabin' TEXT,'Embarked' TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
