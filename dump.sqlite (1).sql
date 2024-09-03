55ef71436987d95d436ce5a363c253cd67374ae9
auroraloreti <loretiaurora16@gmail.com>
2024-09-03 19:36:11 +0200

    esercizio modificato

--- dump.sqlite (1).sql
+++ dump.sqlite (1).sql
@@ -1,10 +1,24 @@
--- TABLE
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
-CREATE TABLE 'Titanic' ('PassengerId' INTEGER,'Survived' INTEGER,'Pclass' INTEGER,'Name' TEXT,'Sex' TEXT,'Age' REAL,'SibSp' INTEGER,'Parch' INTEGER,'Ticket' TEXT,'Fare' REAL,'Cabin' TEXT,'Embarked' TEXT);
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+-- 1. Visualizza le passeggere di sesso femminile che sono sopravvissute e hanno più di 30 anni.
+SELECT * 
+FROM tested
+WHERE Sex = 'female' AND Survived = 1 AND Age > 30;
+
+-- 2. Trova l'età media degli uomini che non sono sopravvissuti.
+SELECT AVG(Age) 
+FROM tested
+WHERE Sex = 'male' AND Survived = 0;
+
+-- 3. Visualizza le informazioni dei passeggeri che hanno speso tra $20 e $50 per i loro biglietti e sono saliti a bordo della nave al porto 'C'.
+SELECT * 
+FROM tested
+WHERE Fare BETWEEN 20 AND 50 AND Embarked = 'C';
+
+-- 4. Trova il numero totale dei sopravvissuti in prima classe
+SELECT COUNT(*) 
+FROM tested
+WHERE Survived = 1 AND Pclass = 1;
+
+-- 5. Mostra le informazioni dei passeggeri che sono saliti a bordo da Cherbourg (porto 'C') e hanno speso più di $75 per i loro biglietti
+SELECT * 
+FROM tested
+WHERE Embarked = 'C' AND Fare > 75;
